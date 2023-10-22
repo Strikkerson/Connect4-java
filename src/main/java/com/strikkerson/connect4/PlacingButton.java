f1188093bad7d5bd601c578ec628cd827b67cf5f
Guilherme Magalhães <107112139+Strikkerson@users.noreply.github.com>
2023-10-21 20:42:09 -0400

    Update PlacingButton.java
    remove netbeans leftover stuff

--- src/main/java/com/strikkerson/connect4/PlacingButton.java
+++ src/main/java/com/strikkerson/connect4/PlacingButton.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.strikkerson.connect4;
 
 import javax.swing.JButton;
 
-/**
- *
- * @author Aluno
- */
+
 public class PlacingButton extends JButton{
     public PlacingButton(){
         super("Jogar");
