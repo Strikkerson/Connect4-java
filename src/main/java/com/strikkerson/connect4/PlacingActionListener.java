96b3d6ee6d377e5132cba03e4ba479ecc57ce905
Guilherme Magalhães <107112139+Strikkerson@users.noreply.github.com>
2023-10-21 20:42:50 -0400

    Update PlacingActionListener.java
    remove netbeans leftover stuff

--- src/main/java/com/strikkerson/connect4/PlacingActionListener.java
+++ src/main/java/com/strikkerson/connect4/PlacingActionListener.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.strikkerson.connect4;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- *
- * @author Aluno
- */
+
 public class PlacingActionListener implements ActionListener{
     public int numCol;
     public Game game;
